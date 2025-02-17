099fef99c4b6df7a43c527470fc45ea9d432e1a7
Anna Kuprym <annakupriyanchik99@gmail.com>
2025-02-18 02:56:07 +0300

    Add problem 384 solved

---
+++ medium-level/384.Shuffle_Array.py
@@ -0,0 +1,44 @@
+""""
+Given an integer array nums, design an algorithm to randomly shuffle the array. All permutations of the array should be equally likely as a result of the shuffling.
+
+Implement the Solution class:
+
+Solution(int[] nums) Initializes the object with the integer array nums.
+int[] reset() Resets the array to its original configuration and returns it.
+int[] shuffle() Returns a random shuffling of the array.
+"""
+
+#1
+import random
+
+class Solution:
+
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+        self.original = nums[:]
+
+    def reset(self) -> List[int]:
+        return self.original
+
+    def shuffle(self) -> List[int]:
+        shuffled = self.nums[:]
+        for i in range(len(shuffled) - 1, 0, -1):
+            j = random.randint(0, i)
+            shuffled[i], shuffled[j] = shuffled[j], shuffled[i]
+        return shuffled
+
+
+#2
+class Solution:
+
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+        self.original = nums[:]
+
+    def reset(self) -> List[int]:
+        return self.original
+
+    def shuffle(self) -> List[int]:
+        s = random.sample(self.nums, len(self.nums))
+        return s
+
