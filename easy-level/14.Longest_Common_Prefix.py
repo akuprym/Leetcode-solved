2be1dc3a16025e86583562330bce44d780a9eaa3
Anna Kuprym <annakupriyanchik99@gmail.com>
2025-04-12 22:31:48 +0300

    Add problem 155(Min_Stack) solved

--- easy-level/14.Longest_Common_Prefix.py
+++ easy-level/14.Longest_Common_Prefix.py
@@ -16,3 +16,63 @@ class Solution:
                 if not pref:
                  return ""
         return pref
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, value):
+        new_node = Node(value)
+        new_node.next = self.head
+        self.head = new_node
+
+    def delete(self, value):
+        current = self.head
+        previous = None
+        while current:
+            if current.value == value:
+                if previous:
+                    previous.next = current.next
+                else:
+                    self.head = current.next
+                return True
+            previous = current
+            current = current.next
+        return False
+
+    def search(self, value):
+        current = self.head
+        while current:
+            if current.value == value:
+                return True
+            current = current.next
+        return False
+
+
+
+import heapq
+
+class MedianFinder:
+    def __init__(self):
+        self.small = []  # Max heap (inverted min heap)
+        self.large = []  # Min heap
+
+    def insert(self, num):
+        heapq.heappush(self.small, -num)
+        if self.small and self.large and -self.small[0] > self.large[0]:
+            heapq.heappush(self.large, -heapq.heappop(self.small))
+        if len(self.small) > len(self.large) + 1:
+            heapq.heappush(self.large, -heapq.heappop(self.small))
+        elif len(self.large) > len(self.small):
+            heapq.heappush(self.small, -heapq.heappop(self.large))
+
+    def find_median(self):
+        if len(self.small) > len(self.large):
+            return -self.small[0]
+        return (-self.small[0] + self.large[0]) / 2
---
+++ grokking_algos_book/00_binary_search.py
@@ -0,0 +1,7 @@
+def binary_search(list, num):
+    low = list[0]
+    high = 0
+    mid = (next + prev) // 2
+
+    while low <= high:
+        if mid > 
